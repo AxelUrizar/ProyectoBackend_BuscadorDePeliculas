d9a7eff189f5e77741a1584a07ad6d0ccbc02f4f
FakeFly <axelurizarmartin@gmail.com>
2022-02-01 18:33:08 +0000

    creación de pedidos terminada

--- routes/orders.js
+++ routes/orders.js
@@ -1,17 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+var funciones = require('../funciones')
+
 // const auth = require('../middlewares/auth');
 
-router.get('/newOrder', (req, res) => {
+router.get('/newOrder', async (req, res) => {
     // let fecha = new Date.now()
-    console.log('hola')
     let date = new Date()
     let expireDate = new Date()
-    res.json({
-        rent_date: date.toLocaleDateString(),
-        expire_date: expireDate.toLocaleDateString(expireDate.setDate(expireDate.getDate() + 7))
+    let listadoParaFiltrar = await funciones.peliculas();
+    let rentedMovie = {}
 
+   listadoParaFiltrar.forEach(element => {
+        if (element.id == req.query.id) {
+            rentedMovie = element.original_title
+        }
+    });
+
+    res.json({
+        rented_movie: rentedMovie,
+        rent_date: date.toLocaleDateString(),
+        rent_time: '7 days',
+        expire_date: expireDate.toLocaleDateString(expireDate.setDate(expireDate.getDate() + 7))
     })
 })
 
